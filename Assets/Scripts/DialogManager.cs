070d82de2a99f30f53fc9f62c117d5eb99ea972a
adrianteh126 <adrianteh02@hotmail.com>
2024-06-11 15:09:35 +0800

    feat: npcs with dialog box

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DialogManager : MonoBehaviour {
+
+    public UnityEngine.UI.Image actorImage;
+    public TextMeshProUGUI actorName;
+    public TextMeshProUGUI messageText;
+    public RectTransform backgroundBox;
+
+    Message[] currentMessages;
+    Actor[] currentActors;
+    int activeMessage = 0;
+    public static bool isActive = false;
+
+    public void OpenDialog(Message[] messages, Actor[] actors) {
+        currentMessages = messages;
+        currentActors = actors;
+        activeMessage = 0;
+        isActive = true;
+        Debug.Log("Started conversation: Loaded messages: " + messages.Length);
+        DisplayMessage();
+        backgroundBox.LeanScale(Vector3.one, 0.25f).setEaseInOutCubic();
+    }
+
+    void DisplayMessage() {
+        Message messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.message;
+
+        Actor actorToDisplay = currentActors[messageToDisplay.actorId];
+        actorImage.name = actorToDisplay.name;
+        actorImage.sprite = actorToDisplay.sprite;
+    }
+
+    public void NextMessage() {
+        activeMessage++;
+        if (activeMessage < currentMessages.Length) {
+            DisplayMessage();
+        }
+        else {
+            Debug.Log("Conversation End");
+            backgroundBox.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+            isActive = false;
+        }
+    }
+
+    public void OnDialog(InputAction.CallbackContext context) {
+        if (!context.started || !isActive) return;
+        NextMessage();
+    }
+
+    void Start() {
+        backgroundBox.transform.localScale = Vector3.zero;
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,59 +1,40 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     Rigidbody2D rb;
     Vector2 moveInput;
     public float moveSpeed;
     Animator animator;
     SpriteRenderer spriteRenderer;
 
-    private void Awake()
-    {
+    private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void OnMove(InputValue value)
-    {
+    private void OnMove(InputValue value) {
         moveInput = value.Get<Vector2>();
 
         // animation
-        if (moveInput == Vector2.zero)
-        {
+        if (moveInput == Vector2.zero) {
             animator.SetBool("isWalking", false);
         }
-        else
-        {
+        else {
             animator.SetBool("isWalking", true);
-            if (moveInput.x > 0)
-            {
+            if (moveInput.x > 0) {
                 spriteRenderer.flipX = false;
             }
-            if (moveInput.x < 0)
-            {
+            if (moveInput.x < 0) {
                 spriteRenderer.flipX = true;
             }
 
         }
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
+        if (DialogManager.isActive == true) return;
         rb.AddForce(moveInput * moveSpeed);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
